7b15b2af54474fa018da1d79299df9528333f4f3
Zadrian <adrian.brunner@gmail.com>
2022-09-19 17:30:26 +0000

    Lesson 1: JavaScript 101

---
+++ yourPlayground.js
@@ -0,0 +1,23 @@
+console.log('hello world')
+console.log('Adria Brunner')
+
+// Variables
+Nickname = 'Zadrian'
+console.log(Nickname)
+
+// Operators
+fruit = prompt('What is your favorite fruit?')
+
+console.log(fruit)
+
+food = Number(prompt('how much was the food?'))
+tip = Number(prompt('tip %?')) / 100
+tipAmount = food * tip
+totalAmount = food + tipAmount
+
+// alert('tip amount: ' + tipAmount)
+console.log('tip amount: ' + tipAmount)
+
+console.log('The ' + fruit + ' costs a total of: ' + totalAmount)
+
+// user input
